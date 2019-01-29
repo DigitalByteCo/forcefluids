095953f5ae13c50f1b6e4d1f6d774efa7ec0b541
Parth Shah <pdshah3690@gmail.com>
2019-01-29 20:02:00 +0530

    add js in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,8 +57,10 @@
         </main>
     </div>
     <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
+
     @yield('script')
 </body>
 </html>
